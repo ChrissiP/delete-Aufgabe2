b4e6c9d4560418c184f5bad2276dfadbd6b92762
ChrissiP <christina.psiloudi@docc.techstarter.de>
2023-01-05 15:20:36 +0100

    Aufgabe 1 json

--- json.py
+++ json.py
@@ -1,6 +1,6 @@
 import json
 
-with open("tsjson/accounts.json") as json_loads:
-    print(type(json_loads))
-    python_liste = json.load(json_loads)
-    print(json.dumps(python_liste, indent=10))
+with open("tsjson/accounts.json") as Datei:
+    print(type(Datei))
+    Liste = json.load(Datei)
+    print(json.dumps(Liste, indent=10))
